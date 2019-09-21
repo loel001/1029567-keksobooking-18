18e24f5b2e309c6343557f7f7de93e92739dbe78
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-09-21 20:56:18 +0300

    Разрешила конфликт

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 'use strict';
 
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+var chooseHousing = function () { 
+    author: {
+        avatar: 'img/avatars/user' + 0 + getRandomInt(1, 8) + '.png'
+    }
+//    offer: {
+//         title: строка, заголовок предложения
+//    }
+//         address: строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
+//         price: число, стоимость
+//         type: строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+//         rooms: число, количество комнат
+//         guests: число, количество гостей, которое можно разместить
+//         checkin: строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
+//         checkout: строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+//         features: массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+//         description: строка с описанием,
+//         photos: массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http:/
+};
