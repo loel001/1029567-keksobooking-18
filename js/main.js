7db193110e66b34a2e3529cb9a764e845aebdc40
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-09-22 22:24:01 +0300

    Исправила задание 3.2

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ var HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TIME_INTERVALS = ['12:00', '13:00', '14:00'];
 var FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTO_ADDRESSES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var HALF_OF_PIN_X = 25;
+var HALF_OF_PIN_Y = 35;
 var NUMBER_OF_OBJECTS = 8;
 var map = document.querySelector('.map');
-var height = map.offsetWidth;
+var widght = map.offsetWidth;
 var similarContainerElement = document.querySelector('.map__pins');
 var similarHousingTemplate = document.querySelector('#pin')
     .content
@@ -34,27 +36,27 @@ var chooseHousing = function () {
       photos: PHOTO_ADDRESSES[Math.floor(Math.random() * PHOTO_ADDRESSES.length)]
     },
     location: {
-      x: getRandomInt(0, height),
-      y: getRandomInt(130, 630)
+      x: getRandomInt(0 + HALF_OF_PIN_X, widght - HALF_OF_PIN_X),
+      y: getRandomInt(130 + HALF_OF_PIN_Y, 630)
     }
   };
 };
 
 map.classList.remove('map--faded');
 
-var chooseTag = function (buf) {
+var getTag = function (pin) {
   var housingElement = similarHousingTemplate.cloneNode(true);
   var photo = housingElement.querySelector('img[src]');
-  housingElement.style = 'left: ' + buf.location.x + 'px; top: ' + buf.location.y + 'px;';
-  photo.src = buf.author.avatar;
-  photo.alt = buf.offer.title;
+  housingElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+  photo.src = pin.author.avatar;
+  photo.alt = pin.offer.title;
   return housingElement;
 };
 
 var getFragment = function () {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < NUMBER_OF_OBJECTS; i++) {
-    fragment.appendChild(chooseTag(chooseHousing()));
+    fragment.appendChild(getTag(chooseHousing()));
   }
   return fragment;
 };
