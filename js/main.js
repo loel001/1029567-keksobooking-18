55075a4a08f0736534b3fee5af8da83f21df8a3c
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-09-22 21:06:29 +0300

    Додела задание 3.2

--- js/main.js
+++ js/main.js
@@ -7,34 +7,56 @@ var HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TIME_INTERVALS = ['12:00', '13:00', '14:00'];
 var FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTO_ADDRESSES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var NUMBER_OF_OBJECTS = 8;
 var map = document.querySelector('.map');
 var height = map.offsetWidth;
+var similarContainerElement = document.querySelector('.map__pins');
+var similarHousingTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
+
 var chooseHousing = function () {
-  var objects = [];
-  for (var i = 0; i < 8; i++) {
-    objects.push({
-      author: {
-        avatar: 'img/avatars/user' + 0 + getRandomInt(1, 8) + '.png'
-      },
-      offer: {
-        title: 'Проверенное жильё',
-        address: '600, 350',
-        price: 3000,
-        type: HOUSING_TYPES[Math.floor(Math.random() * HOUSING_TYPES.length)],
-        rooms: 3,
-        guests: 10,
-        checkin: TIME_INTERVALS[Math.floor(Math.random() * TIME_INTERVALS.length)],
-        checkout: TIME_INTERVALS[Math.floor(Math.random() * TIME_INTERVALS.length)],
-        features: FACILITIES[Math.floor(Math.random() * FACILITIES.length)],
-        description: '',
-        photos: PHOTO_ADDRESSES[Math.floor(Math.random() * PHOTO_ADDRESSES.length)]
-      },
-      location: {
-        x: getRandomInt(0, height),
-        y: getRandomInt(130, 630)
-      }
-    });
-  }
-  return objects;
+  return {
+    author: {
+      avatar: 'img/avatars/user' + 0 + getRandomInt(1, 8) + '.png'
+    },
+    offer: {
+      title: 'Проверенное жильё',
+      address: '600, 350',
+      price: 3000,
+      type: HOUSING_TYPES[Math.floor(Math.random() * HOUSING_TYPES.length)],
+      rooms: 3,
+      guests: 10,
+      checkin: TIME_INTERVALS[Math.floor(Math.random() * TIME_INTERVALS.length)],
+      checkout: TIME_INTERVALS[Math.floor(Math.random() * TIME_INTERVALS.length)],
+      features: FACILITIES[Math.floor(Math.random() * FACILITIES.length)],
+      description: '',
+      photos: PHOTO_ADDRESSES[Math.floor(Math.random() * PHOTO_ADDRESSES.length)]
+    },
+    location: {
+      x: getRandomInt(0, height),
+      y: getRandomInt(130, 630)
+    }
+  };
 };
+
 map.classList.remove('map--faded');
+
+var chooseTag = function (buf) {
+  var housingElement = similarHousingTemplate.cloneNode(true);
+  var photo = housingElement.querySelector('img[src]');
+  housingElement.style = 'left: ' + buf.location.x + 'px; top: ' + buf.location.y + 'px;';
+  photo.src = buf.author.avatar;
+  photo.alt = buf.offer.title;
+  return housingElement;
+};
+
+var getFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < NUMBER_OF_OBJECTS; i++) {
+    fragment.appendChild(chooseTag(chooseHousing()));
+  }
+  return fragment;
+};
+
+similarContainerElement.appendChild(getFragment());
