f527734b6a21e4b4663f2f386f5bc5835882ddc7
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-09-22 22:39:06 +0300

    Последние изменения

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ var HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TIME_INTERVALS = ['12:00', '13:00', '14:00'];
 var FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTO_ADDRESSES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var HALF_OF_PIN_X = 25;
-var HALF_OF_PIN_Y = 35;
+var PIN_X = 50;
+var PIN_Y = 70;
 var NUMBER_OF_OBJECTS = 8;
 var map = document.querySelector('.map');
-var widght = map.offsetWidth;
+var widghtMap = map.offsetWidth;
 var similarContainerElement = document.querySelector('.map__pins');
 var similarHousingTemplate = document.querySelector('#pin')
     .content
@@ -36,8 +36,8 @@ var chooseHousing = function () {
       photos: PHOTO_ADDRESSES[Math.floor(Math.random() * PHOTO_ADDRESSES.length)]
     },
     location: {
-      x: getRandomInt(0 + HALF_OF_PIN_X, widght - HALF_OF_PIN_X),
-      y: getRandomInt(130 + HALF_OF_PIN_Y, 630)
+      x: getRandomInt(0 + PIN_X, widghtMap - PIN_X),
+      y: getRandomInt(130 + PIN_Y, 630)
     }
   };
 };
