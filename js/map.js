2558de3f506c5c11dae094bf9e42a9baefa09a70
Ostretsova Anna <ostretsova.ann@gmail.com>
2019-10-05 11:46:36 +0300

    Начала задание 5.12

--- js/map.js
+++ js/map.js
@@ -20,11 +20,57 @@
     similarContainerElement.appendChild(window.cards.getFragment(window.cards.objects));
   };
 
-  window.util.mapPin.addEventListener('mousedown', function () {
+  window.util.mapPin.addEventListener('mousedown', function (evt) {
     var avatars = document.querySelectorAll('.map__pin:not(.map__pin--main)');
     if (avatars.length === 0) {
+      evt.preventDefault();
       addUponActivation();
       window.cards.openPopupAvatar(window.cards.objects);
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var dragged = false;
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        dragged = true;
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        window.util.mapPin.style.top = (window.util.mapPin.offsetTop - shift.y) + 'px';
+        window.util.mapPin.style.left = (window.util.mapPin.offsetLeft - shift.x) + 'px';
+
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+
+        if (dragged) {
+          var onClickPreventDefault = function (jEvt) {
+            jEvt.preventDefault();
+            window.util.mapPin.removeEventListener('click', onClickPreventDefault);
+          };
+          window.util.mapPin.addEventListener('click', onClickPreventDefault);
+        }
+
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
     }
   });
 
